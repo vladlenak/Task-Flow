ee8703b69174b0b0170fefd3613fd77608d34bb8
vladlenakhtemov <vladlenahtemov@gmail.com>
2024-07-14 01:27:00 +0200

    Room refactoring.

--- app/src/main/java/t/me/octopusapps/taskflow/data/local/db/TaskDatabase.kt
+++ app/src/main/java/t/me/octopusapps/taskflow/data/local/db/TaskDatabase.kt
@@ -1,33 +1,9 @@
 package t.me.octopusapps.taskflow.data.local.db
 
-import androidx.room.Dao
 import androidx.room.Database
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.Update
 import t.me.octopusapps.taskflow.data.local.models.Task
-
-@Dao
-interface TaskDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(task: Task)
-
-    @Update
-    suspend fun update(task: Task)
-
-    @Delete
-    suspend fun delete(task: Task) // New delete method
-
-    @Query("SELECT * FROM tasks WHERE id = :id")
-    suspend fun getTaskById(id: Int): Task?
-
-    @Query("SELECT * FROM tasks")
-    suspend fun getAllTasks(): List<Task>
-}
 
 @Database(entities = [Task::class], version = 1)
 @TypeConverters(PriorityConverter::class)
